461317a85bda8f2260be221cc754e125b68ee4ff
Noah Allan <noahallan33@gmail.com>
2021-02-20 21:22:33 +0000

    Track Data

--- track.py
+++ track.py
@@ -6,22 +6,3 @@ section_c = ['6c', '7c', '8c', '9c', '10c', '11c', '12c',
              '13c', '14c', '15c', '16c', '17c', '18c',
              '19c', '20c', '21c', '22c', '23c', '24c', '25c']
 section_d = ['26', '27', '28', '29', '30']
-import tkinter
-from tkinter import *
-from PIL import Image, ImageTk
-
-root = Tk()
-
-# Create a photoimage object of the image in the path
-image1 = Image.open('C:/Users/noaha/PycharmProjects/NRM/images/NRM_P1.png')
-test = ImageTk.PhotoImage(image1)
-
-label1 = tkinter.Label(image=test)
-label1.image = test
-
-# Position image
-label1.grid(row=0, column=1)
-root.mainloop()
-
-
-
