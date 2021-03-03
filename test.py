cf0b4049e68a2d6c74c26449fc9a5a43989c9779
Noah Allan <noahallan33@gmail.com>
2021-03-03 09:52:11 +0000

    Download GUI screen complete.

--- test.py
+++ test.py
@@ -33,48 +33,13 @@
 #
 # print(bool_change(data[0]))
 
-import time
-
-sensor_data: list[bool] = [False, False, False, False, False, False]
-signal_data: list[bool] = [False, False, False, False, False, False]
+import datetime
 
 
-def signal_check():
-    if sensor_data[0] is True:
-        signal_data[0] = True
-        signal_data[1] = False
-        signal_data[2] = False
-    if sensor_data[1] is True:
-        signal_data[0] = False
-        signal_data[1] = False
-        signal_data[2] = False
-    if sensor_data[2] is True:
-        signal_data[0] = False
-        signal_data[1] = True
-        signal_data[2] = False
-    if sensor_data[3] is True:
-        signal_data[3] = True
-        signal_data[4] = False
-        signal_data[5] = False
-    if sensor_data[4] is True:
-        signal_data[3] = False
-        signal_data[4] = False
-        signal_data[5] = False
-    if sensor_data[5] is True:
-        signal_data[3] = False
-        signal_data[4] = False
-        signal_data[5] = True
-    print(f'{signal_data}')
-    # sensor_data[0] = False
-    # sensor_data[1] = False
-    # sensor_data[2] = False
-    # sensor_data[3] = False
-    # sensor_data[4] = False
-    # sensor_data[5] = False
+def find_date_file_name():
+    x = datetime.datetime.today()
+    date_file = x.strftime('_%d-%m-%Y_%H:%M:%S')
+    return date_file
 
 
-for x in range(30):
-    sensor_data[2] = True
-    sensor_data[3] = True
-    signal_check()
-    time.sleep(1)
+print(find_date_file_name())
